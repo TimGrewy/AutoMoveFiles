bf8df3a4b8f7682bd287432945d0b56811da0b75
Tim Grewy <grewy@hotmail.com>
2015-06-05 10:49:43 +0200

    Changed program to look for 101 in "bad file names"

--- AutoMoveFiles/src/org/dk/tim/file/RegexTool.java
+++ AutoMoveFiles/src/org/dk/tim/file/RegexTool.java
@@ -5,7 +5,8 @@ import java.util.regex.Pattern;
 
 public class RegexTool {
 	private static final String REGEX_SEASON_AND_EPISODE_GROUPING = ".*S(?<season>\\d{1,2})E(?<episode>\\d{1,2}).*";
-	private static final String REGEX_COMBINED_SEASON_AND_EPISODE_GROUPING = ".*(?<combined>\\d{4}).*"; //Eg. 0101<->S01E01 
+	private static final String REGEX_COMBINED_SEASON_AND_EPISODE_GROUPING_4_DIGITS = ".*(?<combined>\\d{4}).*"; //Eg. 0101<->S01E01 
+	private static final String REGEX_COMBINED_SEASON_AND_EPISODE_GROUPING_3_DIGITS = ".*(?<combined>\\d{3}).*"; //Eg. 101<->S01E01 
 
 	public static String extractSeasonFromFileName(String fileName) {
 		Pattern pattern = Pattern.compile(REGEX_SEASON_AND_EPISODE_GROUPING, Pattern.CASE_INSENSITIVE);
@@ -17,13 +18,27 @@ public class RegexTool {
 		return null;
 	}
 
+	/**
+	 *	Looks for files names 0101 OR 101 and changes them to s01e01 
+	 */
 	public static String generateCorrectFileName(String fileName) {
-		Pattern pattern = Pattern.compile(REGEX_COMBINED_SEASON_AND_EPISODE_GROUPING, Pattern.CASE_INSENSITIVE);
-		Matcher matcher = pattern.matcher(fileName);
+		//Look for 0101
+		Pattern pattern1 = Pattern.compile(REGEX_COMBINED_SEASON_AND_EPISODE_GROUPING_4_DIGITS, Pattern.CASE_INSENSITIVE);
+		Matcher matcher1 = pattern1.matcher(fileName);
 
-		if (matcher.matches()) {
-			String oldNotation = matcher.group("combined");
+		if (matcher1.matches()) {
+			String oldNotation = matcher1.group("combined");
 			String newSeasonAndEpisodeNotation = "S" + oldNotation.substring(0, 2) + "E" + oldNotation.substring(2, 4);
+
+			return fileName.replace(oldNotation, newSeasonAndEpisodeNotation);
+		}
+		//look for 101
+		Pattern pattern2 = Pattern.compile(REGEX_COMBINED_SEASON_AND_EPISODE_GROUPING_3_DIGITS, Pattern.CASE_INSENSITIVE);
+		Matcher matcher2 = pattern2.matcher(fileName);
+
+		if (matcher2.matches()) {
+			String oldNotation = matcher2.group("combined");
+			String newSeasonAndEpisodeNotation = "S0" + oldNotation.substring(0, 1) + "E" + oldNotation.substring(1, 3);
 
 			return fileName.replace(oldNotation, newSeasonAndEpisodeNotation);
 		}
--- AutoMoveFiles/test/org/dk/tim/file/RegexToolTest.java
+++ AutoMoveFiles/test/org/dk/tim/file/RegexToolTest.java
@@ -10,6 +10,10 @@ public class RegexToolTest {
 		String family = "family.guy.1318.hdtv-lol.mp4";
 		String fileName = RegexTool.generateCorrectFileName(family);
 		System.out.println(fileName);
+
+		String family1 = "family.guy.118.hdtv-lol.mp4";
+		String fileName1 = RegexTool.generateCorrectFileName(family1);
+		System.out.println(fileName1);
 	}
 
 }
